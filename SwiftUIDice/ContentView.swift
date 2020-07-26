275b4eb48e58c3e53afb9854053fd501401b882a
Pritivi Chhabria Sachin <pritivischhabria@berkeley.edu>
2020-07-26 22:50:12 +0530

    Project Complete

--- SwiftUIDice/ContentView.swift
+++ SwiftUIDice/ContentView.swift
@@ -9,13 +9,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDice = 1
+    @State var rightDice = 2
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDice)
+                    DiceView(n: rightDice)
+                }
+                Spacer()
+                Button(action: {
+                    self.leftDice = Int.random(in: 1...6)
+                    self.rightDice = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 40))
+                        .fontWeight(.heavy)
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white)
+                }
+                .background(Color.red)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding(.horizontal)
     }
 }
